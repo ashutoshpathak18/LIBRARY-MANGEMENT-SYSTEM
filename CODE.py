c902ac9ee86f1225bde96739bcd35f6ea8344046
ashutoshpathak18 <pathakashutosh679@gmail.com>
2026-01-05 23:37:38 +0530

    Create CODE.py

---
+++ CODE.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from datetime import date, datetime
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="librarys"
+)
+cur=conn.cursor()
+
+def librarian_register():
+    u = input("Enter username :")
+    p = input("Enter password :")
+    cur.execute("insert into librarian (username,password) values (%s,%s)", (u,p))
+    conn.commit()
+    print("Librarian registered successfully")
+
+def librarian_login():
+    u= input("Enter username :")
+    p= input("Enter the password :")
+    cur.execute("select * from librarian where username=%s and password=%s", (u,p))
+    return cur.fetchone() is not None
