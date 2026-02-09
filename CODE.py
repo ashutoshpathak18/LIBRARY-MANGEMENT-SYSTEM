0815200b48d67c41e0f0006cb6bfc948b54ccb47
ashutoshpathak18 <pathakashutosh679@gmail.com>
2026-02-10 00:02:28 +0530

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -84,4 +84,73 @@ def borrow_book():
     member_id= int(input("Enter member ID :"))
     book_id= int(input("Enter book ID :"))
     borrow_date= date.today()
-    cur.execute("insert into borrowings")
+    cur.execute("insert into borrowings (member_id, book_id, borrow_date) values (%s,%s,%s)", (member_id,book_id,borrow_date))
+    conn.commit()
+    print("Book borrowed successfully...!")
+
+def return_book():
+    member_id= int(input("Enter member ID :"))
+    book_id= int(input("Enter book ID :"))
+    return_date= date.today()
+    cur.execute("update borrowings set return_date=%s where member_id=%s and book_id=%s and return_date is NULL", (return_date,member_id,book_id))
+    conn.commit()
+    print("Book returned successfully...!")
+
+def view_borrowings():
+    cur.execute("select * from borrowings")
+    borrowings=cur.fetchall()
+    for borrowing in borrowings:
+        print(f"ID: {borrowing[0]}, Member ID: {borrowing[1]}, Book ID: {borrowing[2]}, Borrow Date: {borrowing[3]}, Return Date: {borrowing[4]}")
+    
+# Close the connection when done
+
+def close_connection():
+    cur.close()
+    conn.close()
+    database="library"
+    print("Connection closed....")
+
+if __name__ == "__main__":
+
+    librarian_register()
+    if librarian_login():
+        print("Librarian logged in successfully\n")
+        print("Press 1 to view book")
+        print("Press 2 to add books")
+        print("Press 3 to Update book")
+        print("Press 4 to Delete book")
+        print("Press 5 to Register member")
+        print("Press 6 to View members")
+        print("Press 7 to Delete member")
+        print("Press 8 to Borrow book")
+        print("Press 9 to Return book")
+        print("Press 10 to View borrowings")
+        print("Press 11 to Close connection")
+        choice= int(input())
+        if choice==1:
+            view_books()
+        elif choice==2:
+            add_book()
+        elif choice==3:
+            update_book()
+        elif choice==4:
+            delete_book()
+        elif choice==5:
+            member_register()
+        elif choice==6:
+            view_members()
+        elif choice==7:
+            delete_member()
+        elif choice==8:
+            borrow_book()
+        elif choice==9:
+            return_book()
+        elif choice==10:
+            view_borrowings()
+        elif choice==11:
+            close_connection()
+        else:
+            print("Invalid choice")
+    
+
+
